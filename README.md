35f960c110ea192dca9d69114d38fe245ec768ea
Marek Czyz <mczyz@ra.rockwell.com>
2016-11-28 07:31:22 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TestDb
+A test app to check performance of the pouch db hosted in jxcore using express-pouchdb
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+npm install
+jx node_modules/jxc/bin/jxc.bin.js install 0.1.7 --use-url http://jxcore.azureedge.net/jxcore-cordova/0.1.7/release/io.jxcore.node.jx
