db6286d81992085d25f73a52e2cef3433efc4de3
Marek Czyz <mczyz@ra.rockwell.com>
2016-11-28 08:27:24 +0100

    Building with jxcore

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+NVM_NODEJS_ORG_MIRROR=https://jxcore.azureedge.net
+export NVM_NODEJS_ORG_MIRROR
+JX_NPM_JXB=jxb311
+export JX_NPM_JXB
+
 npm install
 jx node_modules/jxc/bin/jxc.bin.js install 0.1.7 --use-url http://jxcore.azureedge.net/jxcore-cordova/0.1.7/release/io.jxcore.node.jx
+
+cd www/jxcore
+jx npm install --no-optional --autoremove "*.gz"
+find . -name "*.gz" -delete
+
+cd ../..
+cordova platform add android
+cordova build android --device
+
+
